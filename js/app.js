cc8d2ba8a4872296acbc7a4f7c34918bad6563a8
beatricemachalica <beatka.macha68@yahoo.fr>
2021-02-04 19:28:20 +0100

    last update

--- js/app.js
+++ js/app.js
@@ -3,3 +3,24 @@
 document.querySelector('.burger').addEventListener("click", function (){
     document.querySelector("nav").classList.toggle("visible");
 });
+
+// JS imag modal
+
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the image and insert it inside the modal
+var img = document.getElementById('myImg');
+var modalImg = document.getElementById("img01");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  modal.style.display = "none";
+}
