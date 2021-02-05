f256b3222839f31883a5f9205d90e77ceef39e8a
beatricemachalica <beatka.macha68@yahoo.fr>
2021-02-05 10:16:56 +0100

    update img modal

--- js/app.js
+++ js/app.js
@@ -4,23 +4,46 @@ document.querySelector('.burger').addEventListener("click", function (){
     document.querySelector("nav").classList.toggle("visible");
 });
 
-// JS imag modal
+// JS one imag modal :
+
+// Get the modal
+// var modal = document.getElementById('myModal');
+
+// Get the image and insert it inside the modal
+// var img = document.getElementById('myImg');
+// var modalImg = document.getElementById("img01");
+// img.onclick = function(){
+//   modal.style.display = "block";
+//   modalImg.src = this.src;
+// }
+
+// Get the <span> element that closes the modal
+// var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+// span.onclick = function() { 
+//   modal.style.display = "none";
+// }
+
+// For multi img modal :
 
 // Get the modal
 var modal = document.getElementById('myModal');
+// Get the images and bind an 
+// onclick event on each to insert it inside the modal
 
-// Get the image and insert it inside the modal
-var img = document.getElementById('myImg');
+var images = document.querySelectorAll(".photoGalerie");
 var modalImg = document.getElementById("img01");
-img.onclick = function(){
-  modal.style.display = "block";
-  modalImg.src = this.src;
+for(let i = 0; i < images.length; i++){
+  images[i].onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+  }
 }
-
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() { 
+span.onclick = function() {
   modal.style.display = "none";
-}
+} 
